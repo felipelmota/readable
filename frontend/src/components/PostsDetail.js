c7a834c8e443f097dd03755b2aed1765a064d934
Felipe Mota <felipelmota@gmail.com>
2018-07-07 16:13:24 -0300

    Adding vote in Post Detail

--- frontend/src/components/PostsDetail.js
+++ frontend/src/components/PostsDetail.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import {
+    Button,
+    Row,
+    Col,
+    Label,
+    Glyphicon
+} from 'react-bootstrap';
 import CommentsList from './CommentsList';
-import { fetchPost, deletePost } from '../actions';
+import { fetchPost, deletePost, voteForPost } from '../actions';
+import { timestampToDate } from '../utils/dateHelper';
 
 class PostsDetail extends Component {
 
     componentWillMount() {
         console.log(this.props)
         this.props.fetchPost(this.props.match.params.id);
+    }
+        
+    componentDidReceiveProps(nextProps) {
+        console.log(nextProps)
     }
 
     deleteButtonPress() {
@@ -19,7 +30,7 @@ class PostsDetail extends Component {
     }
     
     render() {
-        const { post } = this.props;
+        const { post, voteForPost } = this.props;
         if (!post) {
             return <div>Loading...</div>;
         }
@@ -34,12 +45,33 @@ class PostsDetail extends Component {
                 <Button bsStyle="danger" onClick={this.deleteButtonPress.bind(this)}>
                     Delete Post
                 </Button>
-                <h2>{post.title}</h2>
-                <h4>{post.author} <small>{post.category}</small></h4>
-                <p>{post.body}</p>
-                <Link to={`/posts/${post.id}/comments/new`}>
-                    <Button>Add comment</Button>
-                </Link>
+                <Row>
+                    <Col md={12}>
+                        <Col md={8} className="text-left">
+                            <h2>{post.title}<br/><small>Posted by {post.author}</small></h2>
+                            <div className="badge">{timestampToDate(post.timestamp)}</div>
+                            <h4><Label bsStyle="primary">{post.category}</Label></h4>
+                            <p>{post.body}</p>
+                        </Col>
+                        <Col md={4} className="text-right">
+                            <h3><Label bsStyle={post.voteScore < 0 ? "danger": "success"}>{post.voteScore}</Label></h3>
+                            <Button onClick={() => voteForPost(post.id, 'upVote')}>
+                                <Glyphicon glyph="thumbs-up" />
+                            </Button>
+                            <Button onClick={() => voteForPost(post.id, 'downVote')}>
+                                <Glyphicon glyph="thumbs-down" />
+                            </Button>
+                        </Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} className="text-right">
+                      <Link to={`/posts/${post.id}/comments/new`}>
+                            <Button bsStyle="primary">Add comment</Button>
+                        </Link>  
+                    </Col>
+                </Row>
+
                 <CommentsList postId={post.id} />
             </div>    
         );
@@ -50,4 +82,5 @@ function mapStateToProps(state, ownProps) {
     return { post: state.posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchPost, deletePost })(PostsDetail);
+export default connect(mapStateToProps, { fetchPost, deletePost, voteForPost })(PostsDetail);
+
