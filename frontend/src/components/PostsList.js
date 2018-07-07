bfbef538efd1cda92880e8c97b218f12bceab72c
Felipe Mota <felipelmota@gmail.com>
2018-07-07 15:47:22 -0300

    Editing and Deleting posts from the main list

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -3,14 +3,18 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Row, Col, Button, Label, Glyphicon, ListGroup } from 'react-bootstrap';
-import { fetchPosts, voteForPost } from '../actions';
+import { Row, Col, Button, Label, Glyphicon, ListGroup, ButtonGroup } from 'react-bootstrap';
+import { fetchPosts, voteForPost, deletePost } from '../actions';
 import { timestampToDate } from '../utils/dateHelper';
 
 class PostsList extends Component {
     
     componentWillMount() {
         this.props.fetchPosts();
+    }
+    
+    deleteButtonPress(id) {
+        this.props.deletePost(id, () => {});
     }
 
     renderPosts() {
@@ -24,6 +28,17 @@ class PostsList extends Component {
             return _.map(posts, post => {
                 return (
                     <li key={post.id} className="list-group-item">
+                        <ButtonGroup className="pull-right">
+                            <Link to={`/posts/edit/${post.id}`}>
+                                <Button bsStyle="warning" >
+                                    Edit Post
+                                </Button>
+                            </Link>
+                            <Button bsStyle="danger" onClick={() => this.deleteButtonPress(post.id)} >
+                                Delete Post
+                            </Button>
+                        </ButtonGroup>
+
                         <h2>
                             <Link to={`posts/${post.id}`} style={{ textDecoration: 'none' }}>
                                 {post.title}
@@ -71,5 +86,5 @@ function mapStateToProps (state) {
 }
 
 export default connect(mapStateToProps, {
-    fetchPosts, voteForPost
+    fetchPosts, voteForPost, deletePost
 })(PostsList);
